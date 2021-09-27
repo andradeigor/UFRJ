80399c5675b89d1663486e69e115db41d61b7349
Igor <igor@pop-os.localdomain>
2021-09-27 12:23:28 -0300

    test 27/09

--- primeiroPeriodo/Comp1/list_7/Q10.c
+++ primeiroPeriodo/Comp1/list_7/Q10.c
@@ -5,20 +5,25 @@ int merge(int *array, int comeco, int meio, int fim){
     /* os tamanhos serão calculados com base nos índices*/
     /* os topos apontarão para onde cada pilha está olhando*/
     int tamanhoEsquerda,tamanhoDireita,i,topoEsquerda=0,topoDireita=0;
+    int *vetorEsquerda,*vetorDireita;
 
     tamanhoEsquerda = meio-comeco+1;
     tamanhoDireita = fim - meio;
-
-    int vetorEsquerda[tamanhoEsquerda], 
-    vetorDireita[tamanhoDireita];
+    /*alocamos dinâmicamente o espaço para as arrays e liberamos após a execução da função*/
+    vetorEsquerda = (int *) malloc (tamanhoEsquerda*sizeof( int ));
+    vetorDireita = (int *) malloc (tamanhoDireita*sizeof( int )); 
     /*cria as subarrays com base no tamanho da lista original*/
+    /*preenchemos as subarrays com os elementos da lista original, para ordenar */
     for(i=0;i<tamanhoEsquerda;i++){
         vetorEsquerda[i] = *(array + comeco + i);
     }
     for(i=0;i<tamanhoDireita;i++){
         vetorDireita[i] = *(array + meio + 1 + i);
     }
+    /* aqui, ordenamos de comeco até o fim a cada interação só entramos em um dos if, por isso os else ifs e else no final*/
     for(i=comeco;i<=(fim);i++){
+        /* os dois primeiros ifs servem para verificar se já esgotamos a pilha da esquerda ou direita*/
+        /* caso tenhamos esgotado, resta adicionar a pilha oposta ao restante da array original*/
         if(topoEsquerda >= tamanhoEsquerda){
             array[i] = vetorDireita[topoDireita];
             topoDireita ++;
@@ -27,19 +32,26 @@ int merge(int *array, int comeco, int meio, int fim){
             array[i] = vetorEsquerda[topoEsquerda];
             topoEsquerda++;
         }
+        /* aqui, é o caso que nenhum dos topos se esgotou, verificamos se o elemento da esquerda é menor, se for adiciona ele.*/
         else if(vetorEsquerda[topoEsquerda]< vetorDireita[topoDireita]){
             array[i] = vetorEsquerda[topoEsquerda];
             topoEsquerda++;
         }
+        /* aqui seria o caso que o da direita é menor, então adicionamos ele a array original e aumentamos o valor da pilha*/
         else{
             array[i] = vetorDireita[topoDireita];
             topoDireita++;
         }
     }
+    free(vetorEsquerda);
+    free(vetorDireita);
     return 0;
 }
 
 int mergeSort( int *array, int comeco, int fim){
+    /*essa função é uma auxiliar, ela existe para dividir a array maior em subarrays.
+    Inicialmente, a array que ia de [0:n] virou:
+    array1[0:n/2], array2[n/2:0] em notação com ultimo elemento exclusivo*/
     int meio;
     if(comeco<fim){
         meio = (comeco+fim)/2;
@@ -52,11 +64,24 @@ int mergeSort( int *array, int comeco, int fim){
 
 
 int main(){
-    int i,array[10] = {0,5,4,3,2,10,15,11,23,12};
-    mergeSort(array, 0, 9);
-    for(i=0;i<10;i++){
+    int i,n,*array;
+    printf("Por favor, digite o tamanho do vetor: ");
+    scanf ("%d", &n);
+    
+    /* aloca espaco na memoria para n elementos*/
+    array = (int *) malloc (n*sizeof( int ));
+    for ( i = 0; i < n; i++){
+        printf("Por favor, digite o elemento %dº do vetor: ", i+1);
+        scanf("%d", array+i);
+    }
+    /*Chama a função que de fato vai ordenar, passando index do primeiro e do ultimo elemento,
+     uma array de tamanho n tem n-1 como ultimo index*/
+    mergeSort(array, 0, n-1);
+    /*Printa os elementos ordenados.*/
+    for(i=0;i<n;i++){
         printf("%d ", array[i]);
     }
+    free(array);
     printf("\n");
     return 0;
 }
