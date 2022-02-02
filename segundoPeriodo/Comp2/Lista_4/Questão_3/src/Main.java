d8a1002b8ccaf6a194e376249bc237868b24895e
Igor <ig.vp@hotmail.com>
2022-02-02 11:34:46 -0300

    starting question 3

---
+++ segundoPeriodo/Comp2/Lista_4/Questão_3/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        long Numero = 100000;
+        System.out.println(Main.numeroParaTexto(Numero));
+    }
+    public static String numeroParaTexto(long Numero){
+        Integer base = String.valueOf(Numero).length();
+        return "to aqui";
+    }
+
+}
