8f50aa0f21a67b20251748cfcc11a3c5ebbbb61f
Igor <ig.vp@hotmail.com>
2022-06-06 14:55:57 -0300

    pelo amor de deus c caralho pq tu precisa ser assim

--- terceiroPeriodo/EstruturaDeDados/Expressão/expressão.cpp
+++ terceiroPeriodo/EstruturaDeDados/Expressão/expressão.cpp
@@ -45,7 +45,9 @@ char Pilha::pop(){
     }else{
         char value = topo->value;
         if(size == 1){
-            ptno *aux = topo;
+            if(topo){
+                free(topo);
+            }
             topo = new ptno;
             size--;
             return value;
@@ -105,11 +107,11 @@ Pilha InvertePilha(Pilha pilha){
     return PilhaInvertida;
 }
 
-string toPosFix(string expression){
-    Pilha pilha;
+/*string toPosFix(string expression){
+    stack<char> pilha;
     string posfix = "";
     for(int i = 0; i < expression.length(); i++){
-        if(isalnum(expression[i])){
+        if(!isalnum(expression[i])){
             posfix += expression[i];
         }
         else if(expression[i] == '('){
@@ -118,39 +120,117 @@ string toPosFix(string expression){
         
         else if(expression[i] == ')'){
             while(pilha.top() != '('){
-                posfix += pilha.pop();
+                posfix += pilha.top(); pilha.pop();
             }
             pilha.pop();
         }
         else{
-            while(!pilha.isEmpty() && precedencia(pilha.top()) >= precedencia(expression[i])){
-                posfix += pilha.pop();
+            while(!pilha.empty() && precedencia(pilha.top()) >= precedencia(expression[i])){
+                posfix += pilha.top(); pilha.pop();
             }
             pilha.push(expression[i]);
         }
-        while(!pilha.isEmpty()){
-            posfix += pilha.pop();
+        while(!pilha.empty()){
+            posfix += pilha.top(); pilha.pop();
         }
     }
     return posfix;
+}*/
+void infixToPostfix(char *expression[], int expressionSize){
+    stack<char> st; 
+    string result;
+    
+    for (int i = 0; i < expressionSize; i++) {
+        char c = expression[i][0];
+ 
+        // If the scanned character is
+        // an operand, add it to output string.
+        if (isalnum(c)){
+            result += expression[i];
+            result += ' ';
+        }
+ 
+        // If the scanned character is an
+        // ‘(‘, push it to the stack.
+        else if (c == '(')
+            st.push('(');
+ 
+        // If the scanned character is an ‘)’,
+        // pop and to output string from the stack
+        // until an ‘(‘ is encountered.
+        else if (c == ')') {
+            while (st.top() != '(') {
+                result += st.top();
+                result += ' ';
+                st.pop();
+            }
+            st.pop();
+        }
+ 
+        // If an operator is scanned
+        else {
+            while (!st.empty()
+                   && precedencia(expression[i][0]) <= precedencia(st.top())) {
+                if (c == '^' && st.top() == '^')
+                    break;
+                else {
+                    result += st.top();
+                    result += ' ';
+                    st.pop();
+                }
+            }
+            st.push(c);
+        }
+    }
+ 
+    // Pop all the remaining elements from the stack
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+ 
+    cout << result << endl;
 }
 
+void substituteOperators(char *expression, char *result){
+    int i = 0, l=0;
+    
+    while(expression[i] != '\0'){
+            if(operatorToInt(expression[i])){
+                result[l] =  ' ';
+                l++;
+                result[l] =  expression[i];
+                l++;
+                result[l] =  ' ';
+            }else{
+                result[l] = expression[i];
+            }
+        i++;
+        l++;
+    }
+    result[l] = '\0';
+}
+
+
 int main(int argc, char *argv[]){
-    Pilha numbers;
-    char teste[210];
+    stack<char> numbers;
+    char result[100];
+    char teste[100];
+    char *digits[30];
+    char *digit;
+    int i=0;
     cin >> teste;
-    cout << teste << endl;
-    for (int i = 0; i < strlen(teste); i++){
-        cout << teste[i] << endl;
-        numbers.push(teste[i]);
+    substituteOperators(teste, result);
+    digit = strtok(result, " ");
+    while(digit){
+        digits[i] = (char*) malloc(strlen(digit) * sizeof(char));
+        strcpy(digits[i], digit);
+        digit = strtok(NULL, " ");
+        i++;
     }
-    while(!numbers.isEmpty()){
-        cout << numbers.isEmpty() << endl;
-        cout << numbers.top() << endl;
-        numbers.pop();
-    }
+    infixToPostfix(digits, i);
+
     //string posfix = toPosFix(expresion);
     //cout << posfix << endl;
-    
    return 0;
 }
