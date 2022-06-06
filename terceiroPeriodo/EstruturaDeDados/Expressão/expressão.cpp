34910cfe0c71f1988a7fe4fd7a3de9200cd2de8d
Igor <ig.vp@hotmail.com>
2022-06-06 00:48:59 -0300

    aaaaaaaaaaaaaaaaa

--- terceiroPeriodo/EstruturaDeDados/Expressão/expressão.cpp
+++ terceiroPeriodo/EstruturaDeDados/Expressão/expressão.cpp
@@ -2,10 +2,11 @@
 //Dre: 121095736
 #include <iostream>
 #include <string.h>
+#include <stack>
 using namespace std;
 
 typedef struct ptno{
-    float value;
+    char value;
     struct ptno *next;
 } ptno;
 class Pilha{
@@ -13,36 +14,36 @@ class Pilha{
         ptno *topo;
         int size = 0;
     public:
-        int push(float value);
-        float pop();
-        float top();
+        int push(char value);
+        char pop();
+        char top();
         int isEmpty();
 };
 
-int Pilha::push(float value){
+int Pilha::push(char value){
     if(size == 0){
         topo->value = value;
         topo->next = NULL;
         size++;
         return 0;
     }else{
-        ptno *new_node = new ptno;
-        new_node->value = value;
-        new_node->next = topo;
-        topo = new_node;
-        size ++;
+        ptno *new_no = new ptno;
+        new_no->value = value;
+        new_no->next = topo;
+        topo = new_no;
+        size++;
         return 0;
     }
 }
-float Pilha::top(){
+char Pilha::top(){
     return topo->value;
 }
-float Pilha::pop(){
+char Pilha::pop(){
     if(size == 0){
         cout << "Pilha vazia" << endl;
         return 0;
     }else{
-        float value = topo->value;
+        char value = topo->value;
         if(size == 1){
             ptno *aux = topo;
             topo = new ptno;
@@ -51,26 +52,105 @@ float Pilha::pop(){
         }else{
             ptno *aux = topo;
             topo = topo->next;
-            free(aux);
+            if(aux){
+                free(aux);
+            }
             size--;
             return value;
         }
     }
 }
 int Pilha::isEmpty(){
-    return size == 0;
+    if(size == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
-int main(int argc, char const *argv[]){
+int precedencia(char op){
+    if (op == '/' || op == '*')
+        return 2;
+    else if (op == '+' || op == '-')
+        return 1;
+    else
+        return -1;
+}
+
+
+int operatorToInt(char op){
+    switch (op){
+    case '+':
+        return 43;
+    case '-':
+        return 45;
+    case '*':
+        return 42;
+    case '/':
+        return 47;
+    case '(':
+        return 40;
+    case ')':
+        return 41;
+    default:
+        return 0;
+    }
+}
+Pilha InvertePilha(Pilha pilha){
+    Pilha PilhaInvertida;
+    while(!pilha.isEmpty()){
+        PilhaInvertida.push(pilha.pop());
+    }
+    return PilhaInvertida;
+}
+
+string toPosFix(string expression){
     Pilha pilha;
-    pilha.push(1);
-    pilha.push(2);
-    cout << pilha.pop() << endl;
-    cout << pilha.pop() << endl;
-    cout << pilha.pop() << endl;
-    pilha.push(3);
-    cout << "o topo da pilha é " << pilha.top() << endl;
-    cout << "o pop foi" << pilha.pop() << endl;
-    cout << "o tamanho atual é " << pilha.isEmpty() << endl;
-    return 0;
+    string posfix = "";
+    for(int i = 0; i < expression.length(); i++){
+        if(isalnum(expression[i])){
+            posfix += expression[i];
+        }
+        else if(expression[i] == '('){
+            pilha.push(expression[i]);
+        }  
+        
+        else if(expression[i] == ')'){
+            while(pilha.top() != '('){
+                posfix += pilha.pop();
+            }
+            pilha.pop();
+        }
+        else{
+            while(!pilha.isEmpty() && precedencia(pilha.top()) >= precedencia(expression[i])){
+                posfix += pilha.pop();
+            }
+            pilha.push(expression[i]);
+        }
+        while(!pilha.isEmpty()){
+            posfix += pilha.pop();
+        }
+    }
+    return posfix;
+}
+
+int main(int argc, char *argv[]){
+    Pilha numbers;
+    char teste[210];
+    cin >> teste;
+    cout << teste << endl;
+    for (int i = 0; i < strlen(teste); i++){
+        cout << teste[i] << endl;
+        numbers.push(teste[i]);
+    }
+    while(!numbers.isEmpty()){
+        cout << numbers.isEmpty() << endl;
+        cout << numbers.top() << endl;
+        numbers.pop();
+    }
+    //string posfix = toPosFix(expresion);
+    //cout << posfix << endl;
+    
+   return 0;
 }
