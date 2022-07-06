4a3607d1fef28ba2be168e1c83716afc44671fb2
Igor <ig.vp@hotmail.com>
2022-07-05 22:37:04 -0300

    ALA homeworks

--- terceiroPeriodo/EstruturaDeDados/Exercicio/LinkedList.cpp
+++ terceiroPeriodo/EstruturaDeDados/Exercicio/LinkedList.cpp
@@ -21,6 +21,7 @@ class LinkedList{
         void Remove(float value);
         void Print();
         void Reverse();
+        void Search(float value, no *&ant, no *&pt);
 };
 
 void LinkedList::Add(float value){
@@ -73,16 +74,56 @@ void LinkedList::Reverse(){
 
     }
 }
+
+void LinkedList::Search(float value, no *&ant, no *&pt){ 
+    pt = head;
+    if(!pt){
+        cout << "lista vazia!" << endl; return;
+    }
+    ant = NULL;
+    while(pt != NULL && pt->value != value){
+        ant = pt;
+        pt = pt->next;
+    }
+    return;
+}
+void LinkedList::Remove(float value){
+    no *pt = (no *) malloc(sizeof(no));
+    no *ant = (no *) malloc(sizeof(no));
+    if(!pt or !ant){
+        cout << "sem memória" << endl;
+    }
+    this->Search(value, ant, pt);
+    if(pt ==NULL){
+        cout <<" Valor não está na lista" << endl;
+        return; // não tem o valor na lista
+    }
+    if(ant == NULL){
+        cout << "alo" << endl;
+        head = head->next;
+        if(pt){
+            free(pt);
+        }
+    }else{
+        ant->next = pt->next;
+        free(pt);
+    }
+
+}
 int main(int argc, char const *argv[])
 {
     LinkedList list;
-    list.Add(1.);
-    list.Add(2.);
-    list.Add(3.);
-    list.Add(4.);
-    list.Add(5.);
+    list.Add(1);
+    list.Add(2);
+    list.Add(3);
+    list.Add(4);
+    list.Add(5);
     list.Print();
-    list.Reverse();
+    list.Remove(2);
+    list.Print();
+    list.Remove(1);
+    list.Print();
+    list.Remove(5);
     list.Print();
 }
 
