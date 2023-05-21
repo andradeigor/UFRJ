91aed3287e9abab233f5ce2d0189199eb1d84eb4
Igor <ig.vp@hotmail.com>
2023-05-21 00:18:37 -0300

    trabalho feito em aula

---
+++ quintoPeriodo/lexar.py
@@ -0,0 +1,31 @@
+import re 
+
+
+def matches(line, regexes):
+    for regex in regexes:
+        if(regex[0].match(line)):
+            result = regex[0].match(line)
+            corte = result.end()
+            print(f'{regex[1]}: {result.group(0) if regex[1] != "SPACE" else "" }')
+            return line[corte:]
+
+def main():
+    nomeR = "[a-zA-Z_][a-zA-Z_0-9]*"
+    spacesR = "\s+"
+    numberR = "[0-9]+"
+    operadorR = r"=|\.|\*|\(|\)|\[|\]|\+|\/|\-|\%|\:|,|>"
+    stringR = r"r?\"(.*)\"|\'(.*)\'"    
+    string = [re.compile(stringR),"STRING"]
+    nome = [re.compile(nomeR),"NOME"]
+    spaces = [re.compile(spacesR), "SPACE"]
+    operador = [re.compile(operadorR), "OPERATOR"]
+    number = [re.compile(numberR), "NUMBER"]
+    regexes = [string, nome, spaces, operador,number]
+    with open('lexar.py') as f:
+        lines = f.readlines()
+    for line in lines:
+        while(len(line)):
+            line = matches(line, regexes)
+
+if __name__ == "__main__":
+    main()
