314a7da3f1b0512fa83faf545d775f02c882befa
Igor Andrade <ig.vp@hotmail.com>
2023-11-16 09:34:14 -0300

    Update lab9.py

--- sextoPeriodo/concorrente/lab9/lab9.py
+++ sextoPeriodo/concorrente/lab9/lab9.py
@@ -3,34 +3,21 @@ from multiprocessing import cpu_count
 import time
 from math import sqrt
 #funcao que sera executada de forma concorrente
-def ehPrimo(interval):
-    start,end = interval
-    primesFound = 0
-    for n in range(start,end):
-        isCompost = False
-        if (n<=1): 
-            continue
-        elif (n==2): 
-            primesFound+= 1
-            continue
-        elif (n%2==0): 
-            continue
-        for i in range(3,int(sqrt(n)+1),2):
-            if(n%i==0): 
-                isCompost=True
-                break
-        if isCompost == False: 
-            primesFound+=1
-    return primesFound
+def ehPrimo(n):
+    if (n<=1): return 0
+    elif (n==2): return 1
+    elif (n%2==0): return 0
+    for i in range(3,int(sqrt(n)+1),2):
+        if(n%i==0):return 0
+    return 1
 
 if __name__ == '__main__':
     start = time.time()
     N = 1000
     pool = Pool() #por default, cria um processo distinto para cada processador da maquina
-    spacing = N//cpu_count()
-    numbers = [(i*spacing, (i+1) * spacing if i+1 != cpu_count() else N+1) for i in range(cpu_count())]
+    numbers = list(range(1,N+1))
     #map aceita uma funcao e um objeto iteravel, o pool pega cada valor do objeto iteravel e passa para um processo disponivel no poll que aplica a funcao sobre esse valor
     results = pool.map(ehPrimo, numbers)
-    print(sum(results))
+    print(f'De {1} até {N} nós temos {sum(results)} primos.')
     end = time.time()
     print('work took {} seconds'.format(end - start))
