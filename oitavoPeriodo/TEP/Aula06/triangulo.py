7964f7b5ae5b756c5d6907311e8ce6640de76a05
Igor Andrade <ig.vp@hotmail.com>
2024-09-17 10:53:45 -0300

    Create triangulo.py

---
+++ oitavoPeriodo/TEP/Aula06/triangulo.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+def odd(n):
+    k = n**2
+    N = (k+1)//2
+    if(n<=1):
+        print(-1)
+        return 
+    print(int(sqrt(N**2-n**2)),N )
+
+def even(n):
+    m = n//2
+    if(n<=2):
+        print(-1)
+        return
+    n = 1
+    print(m**2-n**2, m**2+n**2)
+
+n = int(input())
+
+if(n%2==0):
+    even(n)
+else:
+    odd(n)
